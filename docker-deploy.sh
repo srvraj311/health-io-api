ec93f3e3b96c5ca148a3a6dbae47ed1c9464b58a
sourabh <sourabhraj311@gmail.com>
2024-03-24 05:21:15 +0530

    Updated deployment script

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -6,8 +6,8 @@ image_name=$1
 image_tag=$2
 
 # Update with your commands for stopping the old container (if needed)
-docker stop health-io-api  # Example, replace with your container name
-
+#docker stop health-io-api  # Example, replace with your container name
+docker stop health-io-api || true && docker rm health-io-api || true
 docker pull $image_name:$image_tag
 
 # Update with your commands for running the new container
